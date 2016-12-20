2e65e13ff0c817571dee656018936f4dc3b32221
salad7 <msalad@uncc.edu>
2016-12-20 13:43:59 -0500

    Initial state

--- app/src/main/java/com/example/mohamed/studentregform/MainActivity.java
+++ app/src/main/java/com/example/mohamed/studentregform/MainActivity.java
@@ -2,12 +2,39 @@ package com.example.mohamed.studentregform;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    SeekBar seekBar;
+    TextView textView;
+    Student s = new Student("Mohamed Salad","msalad@uncc.edu","CCI","Lol",5);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        textView = (TextView) findViewById(R.id.mood);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                s.setMood(i);
+                textView.setText(s.getMood()+"");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/mohamed/studentregform/Student.java
@@ -0,0 +1,64 @@
+package com.example.mohamed.studentregform;
+
+import java.io.Serializable;
+
+/**
+ * Created by mohamed on 12/20/16.
+ */
+
+public class Student implements Serializable{
+    private String name;
+    private String email;
+    private String department;
+    private String state;
+    private int mood;
+
+    Student(String name, String email, String department, String state, int mood){
+        this.name = name;
+        this.email = email;
+        this.department = department;
+        this.state = state;
+        this.mood = mood;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getMood() {
+        return mood;
+    }
+
+    public void setMood(int mood) {
+        this.mood = mood;
+    }
+}
